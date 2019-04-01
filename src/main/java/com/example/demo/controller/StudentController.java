e1f9b83e06bc7ee45158584e95b58d6bf968864c
huangjh <2372635607@qq.com>
2019-04-01 14:34:21 +0800

    springboot tyymeleaf简单模板

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.bean.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @RequestMapping("show")
+    public String show(Model model) {
+        model.addAttribute("str", "hello spring boot thymeleaf");
+        Student student = new Student();
+        student.setId(100);
+        student.setName("张三");
+        model.addAttribute("stu", student);
+
+        java.util.List<Student> students = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Student stu = new Student();
+            stu.setId(i);
+            stu.setName("stu" + i);
+            students.add(stu);
+        }
+        model.addAttribute("students", students);
+        return "student";
+    }
+
+}
